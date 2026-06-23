6d60f2ec9c6c79b7e4c236913d85a285087e79d7
peytonvecchi <74024201+peytonvecchi@users.noreply.github.com>
2026-06-23 17:00:16 -0400

    Add negative lookbehind to extract_markdown_links

--- src/functions.py
+++ src/functions.py
@@ -5,5 +5,5 @@ def extract_markdown_images(text):
     return matches
 
 def extract_markdown_links(text):
-    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    matches = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
     return matches
