3cea6adba9673573466d0f2f9a210369c5dfcae5
peytonvecchi <74024201+peytonvecchi@users.noreply.github.com>
2026-06-25 20:11:02 -0400

    Add split_nodes_links and split_nodes_images

--- src/test_functions.py
+++ src/test_functions.py
@@ -1,5 +1,6 @@
 import unittest
-from functions import extract_markdown_images, extract_markdown_links
+from functions import extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_extract_markdown_images(self):
@@ -13,6 +14,24 @@ class TestTextNode(unittest.TestCase):
             "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
         )
         self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
 
 
 
