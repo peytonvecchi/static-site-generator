c137c2edabf1bbec5f245cddaed4fa7d84ecd0c8
peytonvecchi <74024201+peytonvecchi@users.noreply.github.com>
2026-07-11 17:48:36 -0400

    Refactor test_block.py

--- src/test_block.py
+++ src/test_block.py
@@ -35,32 +35,5 @@ class TestBlock(unittest.TestCase):
         output = block_to_block_type(md)
         self.assertEqual(BlockType.PARAGRAH, output)
 
-    # def test_eq(self):
-    #     node = TextNode("This is a text node", TextType.TEXT)
-    #     node2 = TextNode("This is a text node", TextType.TEXT)
-    #     self.assertEqual(node, node2)
-    
-    # def test_not_eq(self):
-    #     node = TextNode("This is a bold node", TextType.BOLD)
-    #     node2 = TextNode("This is an italic node", TextType.ITALIC)
-    #     self.assertNotEqual(node, node2)
-
-    # def test_url_none_default(self):
-    #     node = TextNode("This is a text node", TextType.TEXT)
-    #     self.assertIs(node.url, "none")
-
-    # def test_text(self):
-    #     node = TextNode("This is a text node", TextType.TEXT)
-    #     html_node = text_node_to_html_node(node)
-    #     self.assertEqual(html_node.tag, None)
-    #     self.assertEqual(html_node.value, "This is a text node")
-
-    # def test_img(self):
-    #     node = TextNode(text="alt text", text_type=TextType.IMAGE, url="assets/images")
-    #     html_node = text_node_to_html_node(node)
-    #     self.assertEqual(html_node.tag, "img")
-    #     self.assertEqual(html_node.value, "")
-
-
 if __name__ == "__main__":
     unittest.main()
